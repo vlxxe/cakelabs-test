a87bfea39f4a6a3689081dc505bbe8d9b982c1b8
vlxxe <tokzic01@gmail.com>
2020-01-30 20:13:45 +0300

    Updates

---
+++ precache-manifest.7e0505f501ed37be1125bfd5e9840586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d43f444d003953c43a520987b2bc5ef",
+    "url": "/cakelabs-test/index.html"
+  },
+  {
+    "revision": "48c3c6aba76877e59a67",
+    "url": "/cakelabs-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d74b38eef53449e99061",
+    "url": "/cakelabs-test/static/js/2.672bf75c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cakelabs-test/static/js/2.672bf75c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48c3c6aba76877e59a67",
+    "url": "/cakelabs-test/static/js/main.38883445.chunk.js"
+  },
+  {
+    "revision": "8bbc0e02f5459d1fdf1f",
+    "url": "/cakelabs-test/static/js/runtime-main.636ecbf8.js"
+  }
+]);
